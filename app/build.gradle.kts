c01b4593d79068e23d08a8f94d6389d97a4db513
Vitaliy-Golikov <vitala.golikovh@yandex.ru>
2026-03-07 21:17:49 +0300

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     application
-    id "org.sonarqube" version "7.2.3.7755"
+    id("org.sonarqube") version "7.2.3.7755"
     checkstyle
     jacoco
     id("com.github.ben-manes.versions") version "0.51.0"
