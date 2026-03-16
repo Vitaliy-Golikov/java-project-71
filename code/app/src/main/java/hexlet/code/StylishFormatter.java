bd7c5bda27baa6c158e47c4a028b8093f033b763
Vitaliy-Golikov <vitala.golikovh@yandex.ru>
2026-03-16 22:20:38 +0300

    x

---
+++ code/app/src/main/java/hexlet/code/StylishFormatter.java
@@ -0,0 +1,42 @@
+package hexlet.code;
+
+import java.util.*;
+
+public class StylishFormatter implements Formatter {
+
+    @Override
+    public String format(List<Node> nodes) {
+        StringBuilder result = new StringBuilder("{\n");
+
+        for (Node node : nodes) {
+            String key = node.getKey();
+
+            switch (node.getStatus()) {
+                case "deleted":
+                    result.append("  - ").append(key).append(": ")
+                            .append(node.getOldValue()).append("\n");
+                    break;
+
+                case "added":
+                    result.append("  + ").append(key).append(": ")
+                            .append(node.getNewValue()).append("\n");
+                    break;
+
+                case "unchanged":
+                    result.append("    ").append(key).append(": ")
+                            .append(node.getOldValue()).append("\n");
+                    break;
+
+                case "changed":
+                    result.append("  - ").append(key).append(": ")
+                            .append(node.getOldValue()).append("\n");
+                    result.append("  + ").append(key).append(": ")
+                            .append(node.getNewValue()).append("\n");
+                    break;
+            }
+        }
+
+        result.append("}");
+        return result.toString();
+    }
+}
---
+++ code/app/src/main/java/hexlet/code/TreeBuilder.java
@@ -0,0 +1,41 @@
+package hexlet.code;
+
+import java.util.*;
+
+public class TreeBuilder {
+    public static List<Node> build(Map<String, Object> data1, Map<String, Object> data2) {
+        Set<String> allKeys = new TreeSet<>();
+        allKeys.addAll(data1.keySet());
+        allKeys.addAll(data2.keySet());
+
+        List<Node> nodes = new ArrayList<>();
+
+        for (String key : allKeys) {
+            boolean firstKey = data1.containsKey(key);
+            boolean secondKey = data2.containsKey(key);
+
+            if (firstKey && !secondKey) {
+                nodes.add(new Node(key, "deleted", data1.get(key)));
+            } else if (!firstKey && secondKey) {
+                nodes.add(new Node(key, "added", data2.get(key)));
+            } else {
+                Object value1 = data1.get(key);
+                Object value2 = data2.get(key);
+
+                // Исправление: безопасное сравнение с учетом null
+                if (value1 == null && value2 == null) {
+                    nodes.add(new Node(key, "unchanged", null));
+                } else if (value1 == null) {
+                    nodes.add(new Node(key, "changed", null, value2));
+                } else if (value2 == null) {
+                    nodes.add(new Node(key, "changed", value1, null));
+                } else if (value1.equals(value2)) {
+                    nodes.add(new Node(key, "unchanged", value1));
+                } else {
+                    nodes.add(new Node(key, "changed", value1, value2));
+                }
+            }
+        }
+        return nodes;
+    }
+}
