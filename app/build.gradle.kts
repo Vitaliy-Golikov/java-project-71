18d56b849545e083bca484b652389ec17f94e17a
Vitaliy-Golikov <vitala.golikovh@yandex.ru>
2026-03-17 14:52:58 +0300

    x

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
     mavenCentral()
 }
 
+
 dependencies {
     implementation("info.picocli:picocli:4.7.6")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
@@ -42,14 +43,12 @@ tasks.test {
     testLogging {
         events("passed", "skipped", "failed")
     }
-    finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
     reports {
         xml.required.set(true)
-        xml.outputLocation.set(file("${buildDir}/reports/jacoco/test/jacocoTestReport.xml"))
         csv.required.set(false)
         html.required.set(true)
     }
@@ -71,10 +70,5 @@ sonar {
     properties {
         property("sonar.projectKey", "Vitaliy-Golikov_java-project-71")
         property("sonar.organization", "vitaliy-golikov")
-        property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.coverage.jacoco.xmlReportPaths", "${buildDir}/reports/jacoco/test/jacocoTestReport.xml")
-        property("sonar.sources", "src/main/java")
-        property("sonar.tests", "src/test/java")
-        property("sonar.java.binaries", "${buildDir}/classes/java/main")
     }
 }
